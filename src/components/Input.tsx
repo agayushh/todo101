c934185756d43e7a4b9522dc0c6972a0d3bb0ed3
agayushh <agayush088@gmail.com>
2025-06-30 17:37:04 +0530

    feat: add and delete todo

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,24 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 const Input = () => {
   const [todoInput, setTodoInput] = useState("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const addTodo = useCallback(() => {
+    const trimmedInput = todoInput.trim();
+    if (trimmedInput && !todos.includes(trimmedInput)) {
+      setTodos((prev) => [...prev, todoInput]);
+    }
+    setTodoInput("");
+  }, [todos, todoInput]);
+
+  const deleteTodo = useCallback((indexToDelete: number) => {
+    setTodos((prev) =>
+      prev.filter((_, index) => {
+        return index !== indexToDelete;
+      })
+    );
+  }, []);
 
   return (
     <div className="outline-none rounded-md w-[90%] ml-6 mt-5 flex items-center justify-between bg-transparent outline-[#d1d5db] outline-1 focus-within:outline-[#818cf8] focus:outline-2">
@@ -14,7 +31,10 @@ const Input = () => {
         }}
         className="p-2 w-full bg-transparent outline-none text-[#d1d5db]"
       />
-      <button className="bg-[#818cf8] text-[#0f172a] font-medium rounded-md p-2 w-20">
+      <button
+        className="bg-[#818cf8] text-[#0f172a] font-medium rounded-md p-2 w-20"
+        onClick={addTodo}
+      >
         Add
       </button>
     </div>
