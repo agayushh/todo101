0e4f5d36867e887155ac3ba3a7ab05997a5b936c
agayushh <agayush088@gmail.com>
2025-07-07 19:00:15 +0530

    used todo component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from "react";
+import Todo from "./Todo";
 
 const Input = () => {
   const [todoInput, setTodoInput] = useState("");
@@ -17,40 +18,43 @@ const Input = () => {
       prev.filter((_, index) => {
         return index !== indexToDelete;
       })
-    ); 
+    );
   }, []);
 
   return (
     <div>
- <div className="outline-none rounded-md w-[90%] ml-6 mt-5 flex items-center justify-between bg-transparent outline-[#d1d5db] outline-1 focus-within:outline-[#818cf8] focus:outline-2">
-      <input
-        type="text"
-        placeholder="Enter todo"
-        value={todoInput}
-        onChange={(e) => {
-          setTodoInput(e.target.value);
-        }}
-        className="p-2 w-full bg-transparent outline-none text-[#d1d5db]"
-      />
-      <button
-        className="bg-[#818cf8] text-[#0f172a] font-medium rounded-md p-2 w-20"
-        onClick={()=>{addTodo()}}
-      >
-        Add
-      </button>
-    </div>
-      <div>
+      <div className="outline-none rounded-md w-[90%] ml-6 mt-5 flex items-center justify-between bg-transparent outline-[#d1d5db] outline-1 focus-within:outline-[#818cf8] focus:outline-2">
+        <input
+          type="text"
+          placeholder="Enter todo"
+          value={todoInput}
+          onChange={(e) => {
+            setTodoInput(e.target.value);
+          }}
+          className="p-2 w-full bg-transparent outline-none text-[#d1d5db]"
+        />
+        <button
+          className="bg-[#818cf8] text-[#0f172a] font-medium rounded-md p-2 w-20"
+          onClick={() => {
+            addTodo();
+          }}
+        >
+          Add
+        </button>
+      </div>
+      <div className="m-9">
         {todos.map((todo, index) => {
           return (
-            <li key={index}>
-              {todo}
-              <button onClick={()=>{deleteTodo(index)}}>Delete</button>
-            </li>
+            <Todo
+              key={`${todo}-${index}`}
+              todo={todo}
+              index={index}
+              onDelete={deleteTodo}
+            />
           );
         })}
       </div>
     </div>
-   
   );
 };
 
